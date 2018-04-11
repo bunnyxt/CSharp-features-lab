f91eb9dfd1438de6292296a205a41512349ffbe6
bunnyxt <bunnyxt@outlook.com>
2018-04-11 16:34:19 +0800

    Add SwitchConsole class and an example via it.

--- Indexer/Indexer/Program.cs
+++ Indexer/Indexer/Program.cs
@@ -64,6 +64,89 @@ namespace Indexer
         }
     }
 
+    class SwitchConsole
+    {
+        private bool A1, A2, A3;
+        private bool B1, B2, B3;
+        private bool C1, C2, C3;
+
+        public bool this[string index]
+        {
+            get
+            {
+                bool result = false;
+                switch (index)
+                {
+                    case "A1":
+                        result = A1;
+                        break;
+                    case "A2":
+                        result = A2;
+                        break;
+                    case "A3":
+                        result = A3;
+                        break;
+                    case "B1":
+                        result = B1;
+                        break;
+                    case "B2":
+                        result = B2;
+                        break;
+                    case "B3":
+                        result = B3;
+                        break;
+                    case "C1":
+                        result = C1;
+                        break;
+                    case "C2":
+                        result = C2;
+                        break;
+                    case "C3":
+                        result = C3;
+                        break;
+                    default:
+                        break;
+                }
+                return result;
+            }
+            set
+            {
+                switch (index)
+                {
+                    case "A1":
+                        A1 = value;
+                        break;
+                    case "A2":
+                        A2 = value;
+                        break;
+                    case "A3":
+                        A3 = value;
+                        break;
+                    case "B1":
+                        B1 = value;
+                        break;
+                    case "B2":
+                        B2 = value;
+                        break;
+                    case "B3":
+                        B3 = value;
+                        break;
+                    case "C1":
+                        C1 = value;
+                        break;
+                    case "C2":
+                        C2 = value;
+                        break;
+                    case "C3":
+                        C3 = value;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -91,6 +174,14 @@ namespace Indexer
             //find books
             Console.WriteLine("book CLR via C# in lattice " + bookshelf["CLR via C#"]);
             Console.WriteLine("book Introduction to Algorithms in lattice " + bookshelf["Introduction to Algorithms"]);
+
+            //change switch status
+            SwitchConsole switchConsole = new SwitchConsole();
+            string[] switches = { "A1", "B2", "C3" };
+            foreach (var item in switches)
+            {
+                switchConsole[item] = !switchConsole[item];
+            }
         }
     }
 }
