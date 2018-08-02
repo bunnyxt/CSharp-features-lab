76c3331a3333ca46d6a2f7cd6a1f7d514e5dfbda
bunnyxt <bunnyxt@outlook.com>
2018-08-02 14:13:52 +0800

    Add ProcessStart solution.

---
+++ ProcessStart/ProcessStart/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics; //need to using this namespace
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var process = new Process();
+
+            var startInfo = new ProcessStartInfo();
+            startInfo.FileName = "test"; // test is a C# console programe and its code 'Test.cs' is attached as comments
+            startInfo.Arguments = "madoka homura"; // this process need 2 arguments
+            process.StartInfo = startInfo;
+
+            try
+            {
+                Console.WriteLine("Now start process test.exe with arguments madoka and homura.");
+                process.Start();
+
+                Console.WriteLine("Kill the process test.exe after 10 seconds.");
+                for (int i = 10; i > 0; i--)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(1000);
+                }
+
+                Console.WriteLine("Now kill the process test.exe.");
+                process.Kill();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("By.bunnyxt 2018-8-2");
+        }
+    }
+}
---
+++ ProcessStart/ProcessStart/Test.cs
@@ -0,0 +1,29 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(args[0]);
+                Console.WriteLine(args[1]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine(DateTime.Now.ToString());
+            Console.WriteLine("Press enter to close window.");
+            Console.ReadLine();
+        }
+    }
+}
+*/
